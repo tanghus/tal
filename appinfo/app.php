3c66f86d44523216f3bc62f4e8505a6eba4550a8
Thomas Tanghus <thomas@tanghus.net>
2012-07-21 10:37:10 +0200

    Removed obsolete OCP\App::register().

--- appinfo/app.php
+++ appinfo/app.php
@@ -4,8 +4,4 @@ OC::$CLASSPATH['OC_TALL10N'] =					'tal/lib/tall10n.php';
 OC::$CLASSPATH['PHPTAL'] =						'tal/lib/PHPTAL/classes/PHPTAL.php';
 OC::$CLASSPATH['PHPTAL_TranslationService'] =	'tal/lib/PHPTAL/classes/PHPTAL/TranslationService.php';
 
-OCP\App::register( array(
-	'id' => 'tal',
-	'name' => 'TAL Page Templates' ));
-
 OCP\App::registerPersonal('tal','settings');
