8dd7cd4e112508de46970d4f62a24f8c15fc52cd
Thomas Tanghus <thomas@tanghus.net>
2012-11-07 15:43:43 +0100

    Removed obsolete requestlifespan.

--- lib/taltemplate.php
+++ lib/taltemplate.php
@@ -62,12 +62,12 @@ class OC_TALTemplate extends OC_Template {
 		$this->assign('appjspath', OC_App::getAppPath($app).'/js');
 		$this->assign('apptemplatepath', OC_App::getAppPath($app).'/templates');
 		$this->assign('requesttoken', OC_Util::callRegister());
-		$this->assign('requestlifespan', OC_Util::$callLifespan);
+		//$this->assign('requestlifespan', OC_Util::$callLifespan);
 		if($renderas) {
 			$this->assign('maintemplate', OC_App::getAppPath('tal').'/templates/layout.'.$renderas.'.pt');
 			$this->assign('dayNames', json_encode(array((string)$this->i18n->t('Sunday'), (string)$this->i18n->t('Monday'), (string)$this->i18n->t('Tuesday'), (string)$this->i18n->t('Wednesday'), (string)$this->i18n->t('Thursday'), (string)$this->i18n->t('Friday'), (string)$this->i18n->t('Saturday'))));
 			$this->assign('monthNames', json_encode(array((string)$this->i18n->t('January'), (string)$this->i18n->t('February'), (string)$this->i18n->t('March'), (string)$this->i18n->t('April'), (string)$this->i18n->t('May'), (string)$this->i18n->t('June'), (string)$this->i18n->t('July'), (string)$this->i18n->t('August'), (string)$this->i18n->t('September'), (string)$this->i18n->t('October'), (string)$this->i18n->t('November'), (string)$this->i18n->t('December'))));
-			$this->assign('firstDay', json_encode($this->i18n->l('firstday')));
+			$this->assign('firstDay', json_encode($this->i18n->l('firstday', 'firstday')));
 		}
 		//$this->assign('styles', $this->styles);
 		$this->assign('core_styles', !empty(OC_Util::$core_styles)?'core.css':null);
@@ -257,6 +257,10 @@ class OC_TALTemplate extends OC_Template {
 	 * This function proceeds the template and prints its output.
 	 */
 	public function printPage(){
+		// Some headers to enhance security
+		header('X-Frame-Options: Sameorigin');
+		header('X-XSS-Protection: 1; mode=block');
+		header('X-Content-Type-Options: nosniff');
 		echo $this->fetchPage();
 	}
 
