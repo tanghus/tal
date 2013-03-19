2add43da23488addf006d2700126318d4b0a5367
Thomas Tanghus <thomas@tanghus.net>
2013-03-19 21:18:00 +0100

    Load PHPTAL from 3rdparty

--- appinfo/app.php
+++ appinfo/app.php
@@ -1,7 +1,7 @@
 <?php
 OC::$CLASSPATH['OC_TALTemplate'] =				'tal/lib/taltemplate.php';
 OC::$CLASSPATH['OC_TALL10N'] =					'tal/lib/tall10n.php';
-OC::$CLASSPATH['PHPTAL'] =						'tal/lib/PHPTAL/classes/PHPTAL.php';
-OC::$CLASSPATH['PHPTAL_TranslationService'] =	'tal/lib/PHPTAL/classes/PHPTAL/TranslationService.php';
+OC::$CLASSPATH['PHPTAL'] =						'tal/3rdparty/PHPTAL/classes/PHPTAL.php';
+OC::$CLASSPATH['PHPTAL_TranslationService'] =	'tal/3rdparty/PHPTAL/classes/PHPTAL/TranslationService.php';
 
 OCP\App::registerPersonal('tal','settings');
